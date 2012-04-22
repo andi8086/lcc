5627bbdbebc1c31d64f845da9f9c0468137c37c0
Sean W <jbishop137@hotmail.com>
2012-04-22 09:59:17 -0500

    added test cases

---
+++ dcpu16/tst/sort.c
@@ -0,0 +1,55 @@
+#include <dcpulib.h>
+
+int in[] = {10, 32, -1, 567, 3, 18, 1, -51, 789, 0};
+
+main() {
+    unsigned i;
+    unsigned j = (sizeof in)/(sizeof in[0]);
+    sort(in, j);
+    for (i = 0; i < j; i++) {
+        printf("%d\n", in[i]);
+    }
+    return 0;
+}
+
+int *xx;
+
+/* sort - sort a[0..n-1] into increasing order */
+sort(a, n) int a[]; {
+    quick(xx = a, 0, --n);
+}
+
+/* quick - quicksort a[lb..ub] */
+quick(a, lb, ub) int a[]; {
+    int k, partition();
+
+    if (lb >= ub)
+        return;
+    k = partition(a, lb, ub);
+    quick(a, lb, k - 1);
+    quick(a, k + 1, ub);
+}
+
+/* partition - partition a[i..j] */
+int partition(a, i, j) int a[]; {
+    int v, k;
+
+    j++;
+    k = i;
+    v = a[k];
+    while (i < j) {
+        i++; while (a[i] < v) i++;
+        j--; while (a[j] > v) j--;
+        if (i < j) exchange(&a[i], &a[j]);
+    }
+    exchange(&a[k], &a[j]);
+    return j;
+}
+
+/* exchange - exchange *x and *y */
+exchange(x, y) int *x, *y; {
+    int t;
+
+    printf("exchange(%d,%d)\n", x - (unsigned)xx, y - (unsigned)xx);
+    t = *x; *x = *y; *y = t;
+}
